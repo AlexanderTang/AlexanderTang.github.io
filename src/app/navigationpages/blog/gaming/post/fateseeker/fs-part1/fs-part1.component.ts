394acb34350782937780a9ebcb10cb5d3a0d0eae
AlexanderTang <alexanderghtang@gmail.com>
2021-05-06 21:59:48 +0200

    FS part 1

---
+++ src/app/navigationpages/blog/gaming/post/fateseeker/fs-part1/fs-part1.component.ts
@@ -0,0 +1,30 @@
+'use strict';
+import {Component} from '@angular/core';
+import {AbstractBlogPost} from "../../../../../../core/model/AbstractBlogPost";
+import {PostCategory} from "../../../../../../core/enum/postCategory";
+
+@Component({
+    templateUrl: './fs-part1.component.html'
+})
+export class FsPart1Component extends AbstractBlogPost {
+
+    postTitle(): string {
+        return 'Fate Seeker - Let\'s Play Part 1: Meeting the teacher';
+    }
+
+    postDate(): Date {
+        return new Date(2021, 5, 6);
+    }
+
+    postAbstract(): string {
+        return 'Exploring the initial game surroundings and some of the game mechanics such as combat.';
+    }
+
+    postCategories(): PostCategory[] {
+        return [PostCategory.RPG];
+    }
+
+    postImageName(): string {
+        return 'fate_seeker/banner.jpg';
+    }
+}
